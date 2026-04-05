3e9f70800191fc428328df6d2a955afc1ace6b2d
MarguJ <margulesjoa@gmail.com>
2026-04-04 21:25:37 -0300

    Trying tailwind on my phone

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
-import LogInCard from "./components/log in components/LogInCard";
 
 function App() {
   return (
-    <div className="text-center">
-      <LogInCard></LogInCard>
+    <div>
+      <div className="bg-gray-400 py-50 my-50 mx-20">
+        <h1 className="text-xl font-bold text-center text-red-600">
+          Hello World
+        </h1>
+      </div>
     </div>
   );
 }
