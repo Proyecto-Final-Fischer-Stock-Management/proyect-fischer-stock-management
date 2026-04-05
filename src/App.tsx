4dd4880cfa52cc30e733f2d0f220c82be1fbc037
MarguJ <margulesjoa@gmail.com>
2026-04-04 21:27:11 -0300

    Still trying

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <div className="bg-gray-400 py-50 my-50 mx-20">
+      <div className="bg-gray-400 py-50 my-50 mx-10">
         <h1 className="text-xl font-bold text-center text-red-600">
           Hello World
         </h1>
