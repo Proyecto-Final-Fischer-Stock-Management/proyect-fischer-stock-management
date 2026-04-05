f86ff6cbf9ac6edee2b7009fdfff9a8c1f143d8c
MarguJ <margulesjoa@gmail.com>
2026-04-04 23:33:04 -0300

    Top dashboard layout completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,23 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <div className="bg-gray-400 py-50 my-50 mx-10">
-        <h1 className="text-xl font-bold text-center text-red-600">
-          Hello World
-        </h1>
+    <div className="bg-gray-300 min-h-screen">
+      <div className="bg-white min-h-25 flex items-center justify-between">
+        <div
+          style={{ backgroundImage: "url('/LogoFischer.png')" }}
+          className="w-[135px] h-[71px]"
+        ></div>
+        <div className="flex gap-3 pr-3">
+          <div
+            style={{ backgroundImage: "url('/mdi_bell.png')" }}
+            className="w-[24px] h-[24px] bg-cover bg-center"
+          ></div>
+
+          <div
+            style={{ backgroundImage: "url('/mingcute_menu-fill.png')" }}
+            className="w-[25px] h-[25px] bg-cover bg-center"
+          ></div>
+        </div>
       </div>
     </div>
   );
